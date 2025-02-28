96384d5046a5ecda5a4e621bce210f2f5161252f
Mohamad ALayouni <Ayounimoe.visionbios@gmail.com>
2025-02-28 09:07:16 -0800

    Initial Commit

---
+++ FitnessApp/FitnessAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FitnessAppApp.swift
+//  FitnessApp
+//
+//  Created by Mohamad Alayouni on 2/28/25.
+//
+
+import SwiftUI
+
+@main
+struct FitnessAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
