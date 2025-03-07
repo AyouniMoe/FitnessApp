a8f7d83c5a46f9ac84a68aa633a85c3437f14a7c
Mohamad ALayouni <Ayounimoe.visionbios@gmail.com>
2025-03-06 20:39:02 -0800

    section 4.1: Integrate RevenueCat

--- FitnessApp/FitnessApp.swift
+++ FitnessApp/FitnessApp.swift
@@ -5,13 +5,35 @@
 //  Created by Mohamad Alayouni on 2/28/25.
 //
 
+//
+//  FitnessAppApp.swift
+//  FitnessApp
+//
+//  Created by Mohamad Alayouni on 2/28/25.
+//
+
 import SwiftUI
+import RevenueCat
 
 @main
 struct FitnessApp: App {
+    
+    init() {
+        Purchases.logLevel = .debug
+        Purchases.configure(withAPIKey: "appl_GTAYAeABzVycyUzBmhsyDKdPYhz")
+    }
+    
     var body: some Scene {
         WindowGroup {
             FitnessTabView()
+                .onAppear {
+                    Purchases.shared.getOfferings { (offerings, error) in
+                        if let packages = offerings?.current?.availablePackages {
+                            print(packages.first?.offeringIdentifier)
+                        }
+                    }
+                }
         }
     }
 }
+
